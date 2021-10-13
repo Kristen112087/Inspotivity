7783846c13710a6c80836ee65450b591c6307640
Kristen <kristengamelin@gmail.com>
2021-10-13 11:30:09 -0400

    unsaved page from previous commit

--- Inspotivity.Model/PaperPatternModels/PaperPatternItem.cs
+++ Inspotivity.Model/PaperPatternModels/PaperPatternItem.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,11 @@ namespace Inspotivity.Model.PaperPatternModels
         public class PaperPattern
         {
             [Required]
-            public virtual Profile UserId { get; set; }
+            [ForeignKey(nameof(Profile))]
+            public virtual Profile Profile { get; set; }
             public string Designer { get; set; }
             public string PatternName { get; set; }
+
             [DefaultValue(false)]
             public bool HaveMade { get; set; }
         }
